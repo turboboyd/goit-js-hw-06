13d36fa393f8bb036d0d810672cdbfc8667cdf62
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-05-10 18:45:58 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const input = document.querySelector('input');
+const buttonCreate = document.querySelector("[data-create]");
+const boxesEl = document.querySelector("#boxes");
+
+console.log(boxesEl);
+buttonCreate.addEventListener("click", () => {
+  const red = boxesEl.innerHTML = `<div width="30" heigh="30"></div>`; 
+  red * 2;  
+  createBoxes(input.value); 
+});
+
+
+
+function createBoxes(amount) {
+  console.log(amount);
+  boxesEl.innerHTML = 2 * `<div width="30" heigh="30"></div>`; 
+}
+
+// function destroyBoxes(
+
+// )
